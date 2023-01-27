ed301501d780c1ba136c15104ff909aa9513c4c1
Rishabh Jain <rishabhnjain@gmail.com>
2023-01-27 14:45:19 +0530

    added normal vectorsand the scene world

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,6 +1,6 @@
 use crate::{
     hit::{Hit, HitRecord},
-    Point3,
+    Point3, Vec3,
 };
 
 pub struct Sphere {
@@ -34,11 +34,15 @@ impl Hit for Sphere {
             }
         }
 
-        let p = r.at(root);
-        Some(HitRecord {
-            p,
+        let mut rec = HitRecord {
             t: root,
-            normal: (p - self.center) / self.radius,
-        })
+            p: r.at(root),
+            normal: Vec3::new(0.0, 0.0, 0.0),
+            front_face: false,
+        };
+
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, outward_normal);
+        Some(rec)
     }
 }
