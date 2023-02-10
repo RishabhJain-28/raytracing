e125b5b5a6e5deeb0894f5ca4cc682df072a8351
Rishabh Jain <rishabhnjain@gmail.com>
2023-02-10 20:35:27 +0530

    volumes

--- src/constant_medium.rs
+++ src/constant_medium.rs
@@ -1,11 +1,13 @@
-use std::sync::Arc;
+use std::{f64::INFINITY, sync::Arc};
 
-use crate::{Color, Hitable, Isotropic, Material, SolidColor, Texture};
+use rand::Rng;
+
+use crate::{Color, HitRecord, Hitable, Isotropic, Material, SolidColor, Texture, Vec3};
 
 pub struct ConstantMedium<T: Texture> {
     boundary: Box<dyn Hitable>,
     phase_function: Arc<Isotropic<T>>,
-    new_inv_density: f64,
+    neg_inv_density: f64,
 }
 
 impl<T: Texture> ConstantMedium<T> {
@@ -13,7 +15,7 @@ impl<T: Texture> ConstantMedium<T> {
         Self {
             boundary,
             phase_function: Arc::new(Isotropic::new(texture)),
-            new_inv_density: (-1.0 / density),
+            neg_inv_density: (-1.0 / density),
         }
     }
 }
@@ -23,14 +25,61 @@ impl ConstantMedium<SolidColor> {
         Self {
             boundary,
             phase_function: Arc::new(Isotropic::from_color(color)),
-            new_inv_density: (-1.0 / density),
+            neg_inv_density: (-1.0 / density),
         }
     }
 }
 
-impl<T: Texture> Hitable for ConstantMedium<T> {
+impl<T: Texture + 'static> Hitable for ConstantMedium<T> {
     fn bounding_box(&self, time0: f64, time1: f64) -> Option<crate::AABB> {
         self.boundary.bounding_box(time0, time1)
     }
-    fn hit(&self, r: &crate::Ray, t_min: f64, t_max: f64) -> Option<crate::HitRecord> {}
+    fn hit(&self, r: &crate::Ray, t_min: f64, t_max: f64) -> Option<crate::HitRecord> {
+        let mut rec1 = self.boundary.hit(r, -INFINITY, INFINITY)?;
+        let mut rec2 = self.boundary.hit(r, rec1.t + 0.001, INFINITY)?;
+
+        if rec1.t < t_min {
+            rec1.t = t_min;
+        };
+
+        if rec2.t > t_max {
+            rec2.t = t_max;
+        };
+
+        if rec1.t >= rec2.t {
+            return None;
+        };
+
+        if rec1.t < 0.0 {
+            rec1.t = 0.0;
+        };
+
+        let ray_length = r.direction().length();
+        let distance_inside_boundary = (rec2.t - rec1.t) * ray_length;
+        let mut rng = rand::thread_rng();
+        let hit_distance = self.neg_inv_density * rng.gen::<f64>().log10();
+
+        if hit_distance > distance_inside_boundary {
+            return None;
+        };
+
+        let t = rec1.t + hit_distance / ray_length;
+        let p = r.at(t);
+
+        let normal = Vec3::new(1.0, 0.0, 0.0);
+        let front_face = true;
+        let mat: Arc<Isotropic<T>> = self.phase_function.clone();
+
+        let rec = HitRecord {
+            p,
+            t,
+            front_face,
+            normal,
+            mat,
+            u: 0.0,
+            v: 0.0,
+        };
+
+        Some(rec)
+    }
 }
